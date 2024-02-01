4479e7dd51b59d01b1327e26dba47157a19aa18f
AdelinoViegas <buchoteclas@gmail.com>
2024-02-01 19:24:47 +0100

    actualizando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let input = document.querySelector('#input-search');
+let container = document.querySelector('.container-fluid');
+let mobile = document.querySelector('.mobile');
+let menu  = document.querySelector('navbar-collapse');
+let btn = document.querySelector('.close');
 
 input.onfocus = ()=>{
     input.classList.remove('add-Icon-Bag');
@@ -9,3 +13,24 @@ input.onblur = ()=>{
     input.classList.add('add-Icon-Bag');
     input.style.paddingLeft = "45px";
 }
+
+
+
+mobile.addEventListener('click',()=>{
+    container.classList.toggle('add');
+});
+
+
+btn.addEventListener('click',()=>{
+    if(container.getAttribute('class') === 'container-fluid add'){
+        container.classList.remove('add');
+     }
+});
+
+
+
+
+
+
+
+
